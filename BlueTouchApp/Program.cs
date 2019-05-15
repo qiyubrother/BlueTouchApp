8e6d384b057995bb27d75e1299e7735805485527
zhenhua Liu <qiyubrother@outlook.com>
2019-05-15 14:39:52 +0800

    首次提交

--- BlueTouchApp/Program.cs
+++ BlueTouchApp/Program.cs
@@ -14,10 +14,6 @@ namespace BlueTouchApp
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-
             CBlueToothManage btm = new CBlueToothManage();
             do
             {
